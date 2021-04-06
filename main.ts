7ea4878fb7d0eb2e739db19e6e45bf4c873a5731
Tim Riscalas <61475187+s454835@users.noreply.github.com>
2021-04-06 14:15:25 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,25 @@
-input.onButtonPressed(Button.A, function () {
-    music.playMelody("E B C5 A B G A F ", 120)
-})
-input.onButtonPressed(Button.AB, function () {
-    music.playMelody("G B A G C5 B A B ", 120)
-})
-input.onButtonPressed(Button.B, function () {
-    music.playMelody("G F G A - F E D ", 120)
-})
-input.onGesture(Gesture.Shake, function () {
-    music.playMelody("C5 G B A F A C5 B ", 120)
-    basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . # . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . # . #
+        # . . . #
+        # # # # #
+        `)
 })
