031e65797bfe4881ceb958d847a60cb75389dda4
Tim Riscalas <61475187+s454835@users.noreply.github.com>
2021-04-06 14:21:59 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,25 @@
 basic.forever(function () {
     basic.showLeds(`
-        # # # # #
+        . # # # .
         # . . . #
         # . # . #
         # . . . #
-        # # # # #
+        . # # # .
         `)
     basic.pause(2000)
     basic.showLeds(`
         . . . . .
         . . . . .
-        . . # . .
+        # # # # #
         . . . . .
         . . . . .
         `)
     basic.pause(200)
     basic.showLeds(`
-        # # # # #
+        . # # # .
         # . . . #
         # . # . #
         # . . . #
-        # # # # #
+        . # # # .
         `)
 })
